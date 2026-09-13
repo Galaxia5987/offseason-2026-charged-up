26a8ab899a71345687156213a607f56fa12f8ccc
AdarWa <adar.wasserman@egmail.com>
2026-09-13 15:21:06 +0300

    Add subsystem name in abstract roller

--- src/main/kotlin/frc/robot/subsystems/roller/Roller.kt
+++ src/main/kotlin/frc/robot/subsystems/roller/Roller.kt
@@ -31,6 +31,7 @@ abstract class Roller(name: String, val config: RollerConfig) :
                     velocity = true,
                     voltage = true,
                 ),
+            subsystem = name,
         )
 
     protected var setpoint: Voltage = 0.volts
