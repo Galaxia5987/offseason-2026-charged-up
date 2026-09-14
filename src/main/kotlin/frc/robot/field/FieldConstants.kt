99cd9bf004d69cc2eade6a6f6ccf0ded99acc31f
yahali <97111125+yakla@users.noreply.github.com>
2026-09-14 18:44:14 +0300

    update

--- src/main/kotlin/frc/robot/field/FieldConstants.kt
+++ src/main/kotlin/frc/robot/field/FieldConstants.kt
@@ -3,6 +3,7 @@ package frc.robot.field
 import frc.robot.lib.extensions.*
 import org.wpilib.math.geometry.Translation2d
 import org.wpilib.units.measure.Distance
+import java.awt.Color
 
 val CUBE_SIZE = 24.13.cm
 val PLATFORM_SIZE = 143.cm
@@ -11,6 +12,13 @@ enum class TowerXOffset(val offset: Distance) {
     HIGH(PLATFORM_SIZE - 101.cm),
     MID(PLATFORM_SIZE - 58.cm),
     LOW(PLATFORM_SIZE - CUBE_SIZE / 2),
+}
+
+enum class CubeColors(val color: Color?) {
+    RED(Color.RED),
+    YELLO(Color.YELLOW),
+    GREEN(Color.GREEN),
+    NONE(null)
 }
 
 fun getGridOffset(towerXOffset: TowerXOffset) =
