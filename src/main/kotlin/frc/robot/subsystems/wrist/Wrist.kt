bd3bff96829a9da69898aad52360dd095b05fdd5
מאיה <airsignforever@gmail.com>
2026-09-14 17:27:47 +0300

    Add external feedback sensor

--- src/main/kotlin/frc/robot/subsystems/wrist/Wrist.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/Wrist.kt
@@ -1,11 +1,14 @@
 package frc.robot.subsystems.wrist
 
+import com.ctre.phoenix6.CANBus.systemcore
 import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.hardware.CANcoder
 import frc.robot.lib.commands.UnnamedCommand
 import frc.robot.lib.commands.addPeriodic
 import frc.robot.lib.commands.invoke
 import frc.robot.lib.commands.waitUntil
 import frc.robot.lib.extensions.deg
+import frc.robot.lib.extensions.with
 import frc.robot.lib.universal_motor.UniversalTalonFX
 import org.littletonrobotics.junction.Logger
 import org.wpilib.command3.Mechanism
@@ -13,16 +16,21 @@ import org.wpilib.command3.Trigger
 
 object Wrist : Mechanism(), WristPositionCommandFactory {
 
+    private val externalEncoder =
+        CANcoder(ENCODER_PORT, systemcore(0)).apply {
+            configurator.apply(ENCODER_CONFIG)
+        }
+
     private val motor: UniversalTalonFX =
         UniversalTalonFX(
             port = PORT,
             config = CONFIG,
-            gearRatio = RATIO,
+            gearRatio = ROTOR_TO_MECHANISM_RATIO,
             simGains = SIM_GAINS,
-            canCoder = CANcCoder,
         )
 
     var setpoint = 0.deg
+    var state = WristPosition.CLOSED
     val positionVoltage = PositionVoltage(setpoint)
     val atSetpoint = Trigger {
         motor.inputs.position.isNear(setpoint, TOLERANCE)
@@ -30,7 +38,8 @@ object Wrist : Mechanism(), WristPositionCommandFactory {
 
     override fun setTarget(value: WristPosition): UnnamedCommand = this {
         setpoint = value.angle
-        motor.setControl(positionVoltage.withPosition(setpoint))
+        state = value
+        motor.setControl(positionVoltage with setpoint)
         atSetpoint.waitUntil()
     }
 
@@ -40,7 +49,8 @@ object Wrist : Mechanism(), WristPositionCommandFactory {
 
     fun periodic() {
         motor.periodic()
-        Logger.recordOutput("Subsystem/Wrist/setpoint", setpoint)
-        Logger.recordOutput("Subsystem/Wrist/atsetpoint", atSetpoint)
+        Logger.recordOutput("Subsystems/Wrist/setpoint", setpoint)
+        Logger.recordOutput("Subsystems/Wrist/state", state)
+        Logger.recordOutput("Subsystems/Wrist/atsetpoint", atSetpoint)
     }
 }
--- src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
@@ -1,10 +1,12 @@
 package frc.robot.subsystems.wrist
 
-import com.ctre.phoenix6.CANBus
+import com.ctre.phoenix6.configs.CANcoderConfiguration
+import com.ctre.phoenix6.configs.FeedbackConfigs
+import com.ctre.phoenix6.configs.MagnetSensorConfigs
 import com.ctre.phoenix6.configs.MotorOutputConfigs
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
-import com.ctre.phoenix6.hardware.CANcoder
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
 import frc.robot.lib.Gains
@@ -15,14 +17,15 @@ import frc.robot.lib.extensions.rot
 import org.team5987.annotation.command_enum.CommandEnum
 import org.wpilib.units.measure.Angle
 
-val PORT = 1
+const val PORT = 1
+const val ENCODER_PORT = 2
 val SIM_GAINS = Gains(kP = 0.5, kD = 0.075)
 val REAL_GAINS = Gains(kP = 0.5, kD = 0.075)
 val TOLERANCE = 1.deg
-const val RATIO = 0.0
+const val ROTOR_TO_MECHANISM_RATIO = 1.0
+const val SENSOR_TO_MECHANISM_RATIO = 1.0
 val FORWARD_LIMIT = 90.deg
 val REVERSE_LIMIT = 0.deg
-val CANcCoder = CANcoder(1, CANBus())
 val CONFIG =
     TalonFXConfiguration().apply {
         CurrentLimits = createCurrentLimits()
@@ -40,7 +43,22 @@ val CONFIG =
                 ReverseSoftLimitEnable = true
                 ReverseSoftLimitThreshold = REVERSE_LIMIT[rot]
             }
-        CurrentLimits = createCurrentLimits()
+
+        Feedback =
+            FeedbackConfigs().apply {
+                SensorToMechanismRatio = SENSOR_TO_MECHANISM_RATIO
+                RotorToSensorRatio = ROTOR_TO_MECHANISM_RATIO
+                FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder
+                FeedbackRemoteSensorID = ENCODER_PORT
+            }
+    }
+
+val ENCODER_CONFIG =
+    CANcoderConfiguration().apply {
+        MagnetSensor =
+            MagnetSensorConfigs().apply {
+                MagnetOffset = 0.25 // rot
+            }
     }
 
 @CommandEnum
