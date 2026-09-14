17017ad6c2312633e47c6459bb2960f8eb34e042
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-09-14 18:31:10 +0300

    Write scoring low composition

--- src/main/kotlin/frc/robot/states/StateCompositions.kt
+++ src/main/kotlin/frc/robot/states/StateCompositions.kt
@@ -1,5 +1,6 @@
 package frc.robot.states
 
+import frc.robot.drive
 import frc.robot.lib.commands.command
 import frc.robot.lib.commands.unaryPlus
 import frc.robot.subsystems.elevator.Elevator
@@ -30,3 +31,22 @@ fun intaking(): Command = command {
 fun alignment() : Command = command {
     // TODO
 }.named("States/Alignment")
+
+fun scoringLow(): Command = command {
+    drive.lock()
+    if (true) { // todo if cube in intake
+        Wrist.setTarget(WristPosition.OPEN)
+        +IntakeRoller.intake()
+    }
+    else {
+        Wrist.setTarget(WristPosition.CLOSED)
+    }
+
+    +ConveyorRoller.convey()
+    +DispatchRoller.dispatchLow()
+
+    waitUntil { true } // cube leaves body
+
+    // todo unlock drive
+}.named("States/Scoring/Low")
+
