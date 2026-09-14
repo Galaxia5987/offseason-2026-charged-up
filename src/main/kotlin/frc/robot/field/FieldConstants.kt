4d76f05e7190ee7de553c40c1677027f0a6c32ef
yahali <97111125+yakla@users.noreply.github.com>
2026-09-14 17:58:54 +0300

    format

--- src/main/kotlin/frc/robot/field/FieldConstants.kt
+++ src/main/kotlin/frc/robot/field/FieldConstants.kt
@@ -5,12 +5,12 @@ import org.wpilib.math.geometry.Translation2d
 import org.wpilib.units.measure.Distance
 
 val CUBE_SIZE = 24.13.cm
-val platformSize = 143.cm
+val PLATFORM_SIZE = 143.cm
 
 enum class TowerXOffset(val offset: Distance) {
-    HIGH(platformSize - 101.cm),
-    MID(platformSize - 58.cm),
-    LOW(platformSize - CUBE_SIZE / 2),
+    HIGH(PLATFORM_SIZE - 101.cm),
+    MID(PLATFORM_SIZE - 58.cm),
+    LOW(PLATFORM_SIZE - CUBE_SIZE / 2),
 }
 
 fun getGridOffset(towerXOffset: TowerXOffset) =
