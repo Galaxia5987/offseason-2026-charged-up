b235668ccf830ea0bf9ba635ad9033433160064b
Dotan <dotan.hillel@gmail.com>
2026-09-18 12:19:59 +0300

    Formatting

--- src/main/kotlin/frc/robot/states/RollerTriggers.kt
+++ src/main/kotlin/frc/robot/states/RollerTriggers.kt
@@ -1,31 +1,35 @@
 package frc.robot.states
-import org.wpilib.command3.Trigger
-import org.wpilib.command3.Command
-import frc.robot.subsystems.roller.IntakeRoller
+
 import frc.robot.subsystems.roller.ConveyorRoller
-import frc.robot.states.StateMachine
 import frc.robot.subsystems.roller.DispatchRoller
 import frc.robot.subsystems.roller.GripRoller
-import frc.robot.subsystems.sensors.Sensors.bodySensor
-import frc.robot.subsystems.sensors.Sensors.GripSensor
+import frc.robot.subsystems.roller.IntakeRoller
 import frc.robot.subsystems.sensors.Sensors.DispatchSensor
+import frc.robot.subsystems.sensors.Sensors.GripSensor
+import frc.robot.subsystems.sensors.Sensors.bodySensor
+import org.wpilib.command3.Trigger
 
+val intakeRollerStop =
+    Trigger {
+            bodySensor.isPresent && DispatchSensor.isPresent
+        }
+        .whileTrue(IntakeRoller.stop())
 
-val intakeRollerStop = Trigger{
-    bodySensor.isPresent && DispatchSensor.isPresent
-}.whileTrue(IntakeRoller.stop())
+val conveyorRollerStop =
+    Trigger {
+            StateMachine.IDLE
+        }
+        .whileTrue(ConveyorRoller.stop())
 
+val dispatchRollerStop =
+    Trigger {
+            (StateMachine.SCORINGHIGH && GripSensor.isPresent) ||
+                (StateMachine.SCORINGLOW && StateMachine.cubeLeavesBody)
+        }
+        .whileTrue(DispatchRoller.stop())
 
-val conveyorRollerStop = Trigger{
-    StateMachine.IDLE
-}.whileTrue(ConveyorRoller.stop())
-
-
-val dispatchRollerStop = Trigger{
-    (StateMachine.SCORINGHIGH && GripSensor.isPresent) || (StateMachine.SCORINGLOW && StateMachine.cubeLeavesBody)
-}.whileTrue(DispatchRoller.stop())
-
-
-val gripRollerStop = Trigger{
-    (GripSensor.isPresent)
-}.whileTrue(GripRoller.stop())
+val gripRollerStop =
+    Trigger {
+            (GripSensor.isPresent)
+        }
+        .whileTrue(GripRoller.stop())
