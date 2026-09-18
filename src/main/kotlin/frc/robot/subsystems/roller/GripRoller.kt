f79c1ad70cdfe9e6ed55e5090251c95db9934217
AdarWa <adar.wasserman@egmail.com>
2026-09-18 13:00:26 +0300

    Add imports

--- src/main/kotlin/frc/robot/subsystems/roller/GripRoller.kt
+++ src/main/kotlin/frc/robot/subsystems/roller/GripRoller.kt
@@ -5,7 +5,10 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
 import frc.robot.lib.commands.onChange
+import frc.robot.lib.commands.unless
 import frc.robot.lib.extensions.volts
+import frc.robot.subsystems.sensors.Sensors
+import org.wpilib.command3.Command
 import org.wpilib.command3.Trigger
 
 val GRIP_ROLLER_CONFIG =
@@ -30,5 +33,5 @@ object GripRoller : Roller("Grip", GRIP_ROLLER_CONFIG) {
             .named("${name}/grip")
             .unless(Sensors.GripSensor::isPresent)
 
-    fun release() = super.backward().named("${name}/release")
+    fun release(): Command = super.backward().named("${name}/release")
 }
