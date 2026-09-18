edad296065d38c98d9c679e9902b7d000a7b3dae
AdarWa <adar.wasserman@egmail.com>
2026-09-18 11:35:33 +0300

    Use Flipper in `SCORING_POSTS`

--- src/main/kotlin/frc/robot/field/FieldConstants.kt
+++ src/main/kotlin/frc/robot/field/FieldConstants.kt
@@ -2,7 +2,8 @@ package frc.robot.field
 
 import frc.robot.lib.IS_RED
 import frc.robot.lib.extensions.*
-import org.wpilib.math.geometry.Translation2d
+import frc.robot.lib.flipper
+import frc.robot.lib.getPose2d
 import org.wpilib.units.measure.Distance
 
 val CUBE_SIZE = 24.13.cm
@@ -27,11 +28,12 @@ private val SCORING_POST_WIDTH = 1.m
 
 private const val NUM_POSTS = 9
 
-val SCORING_POSTS =
+val SCORING_POSTS = { isRed: Boolean ->
     List(NUM_POSTS) { index ->
-        Translation2d(
+        getPose2d(
             SCORING_POSTS_X,
             SCORING_POSTS_START_OFFSET +
-                (index * (SCORING_POST_WIDTH + SCORING_POST_GAP)[m]).m,
-        )
+                    (index * (SCORING_POST_WIDTH + SCORING_POST_GAP)[m]).m
+        ).flip(isRed)
     }
+}.flipper()
