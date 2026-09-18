a5d843e0bfee9b928d29396bab87454a34309ae6
AdarWa <adar.wasserman@egmail.com>
2026-09-18 12:49:45 +0300

    Fix Sensors.kt

--- src/main/kotlin/frc/robot/subsystems/sensors/Sensors.kt
+++ src/main/kotlin/frc/robot/subsystems/sensors/Sensors.kt
@@ -3,36 +3,40 @@ package frc.robot.subsystems.sensors
 import com.ctre.phoenix6.configs.CANrangeConfiguration
 import frc.robot.field.CubeColors
 import frc.robot.lib.unified_canrange.UnifiedCANRange
-import org.wpilib.util.Color
 
-interface Sensor {
-    val isPresent: Boolean
-    val color: CubeColors get() = null
+interface ColorSensor {
+    val color: CubeColors
 }
+
+interface DistanceSensor {
+    val isPresent: Boolean
+}
+
+interface DistanceColorSensor : DistanceSensor, ColorSensor
 
 object Sensors {
     private val IntakeCanRange = UnifiedCANRange(INTAKE_SENSOR_CANRANGE_PORT, configuration = CANrangeConfiguration())
     private val GripCanRange = UnifiedCANRange(GRIP_CANRANGE_PORT, configuration = CANrangeConfiguration())
 
-    val IntakeSensor = object : Sensor {
+    val IntakeSensor = object : DistanceSensor {
         override val isPresent: Boolean
             get() = IntakeCanRange.isInRange
     }
-    val bodySensor = object : Sensor {
+    val bodySensor = object : DistanceColorSensor {
         override val isPresent: Boolean
             get() = false
         override val color: CubeColors
             get() = CubeColors.NONE
     }
 
-    val DispatchSensor = object : Sensor {
+    val DispatchSensor = object : DistanceColorSensor {
         override val isPresent: Boolean
             get() = false
         override val color: CubeColors
             get() = CubeColors.NONE
 
     }
-    val GripSensor = object : Sensor {
+    val GripSensor = object : DistanceColorSensor {
         override val isPresent: Boolean
             get() = GripCanRange.isInRange
         override val color: CubeColors
