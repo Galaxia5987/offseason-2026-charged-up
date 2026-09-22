cb32a091df589f7af3f629faf7eff06c70dffcc0
asafthedev <asafshotland@gmail.com>
2026-09-22 17:18:20 +0300

    Update to new version

--- src/main/kotlin/frc/robot/states/StateMachine.kt
+++ src/main/kotlin/frc/robot/states/StateMachine.kt
@@ -1,12 +1,10 @@
 package frc.robot.states
 
-import frc.robot.RobotContainer.buttons.intake
-import frc.robot.RobotContainer.buttons.scoring
+import frc.robot.RobotContainer
 import frc.robot.lib.commands.unaryPlus
 import frc.robot.lib.extensions.not
-import frc.robot.lib.state_machine.buildStateMachine
+import frc.robot.lib.state_machine.StateMachineCompanion
 import frc.robot.subsystems.sensors.Sensors
-import org.team5987.annotation.graph.graphgen.GenerateStateMachineGraph
 import org.wpilib.command3.Trigger
 
 enum class State {
@@ -17,43 +15,42 @@ enum class State {
     SCORING_LOW,
     COLOR_CHECK;
 
-    companion object {
-
-        @GenerateStateMachineGraph("State")
-        val stateMachine =
-            buildStateMachine<State>("State Machine") {
-                IDLE(idle())
-                INTAKING(intaking())
-                SCORING_HIGH(scoringHigh())
-                SCORING_LOW(scoringLow())
-                COLOR_CHECK {
-                    park()
-                }
-
-                allOf<State>() on intake switchTo INTAKING
-
-                INTAKING on !intake switchTo IDLE
-
-                IDLE on scoring.trigger switchTo ALIGNMENT
-                // TODO: Add trigger in position for scoring with 'and' operator
-
-                ALIGNMENT on scoring.trigger switchTo IDLE
-
-                ALIGNMENT.onComplete switchTo COLOR_CHECK
-
-                COLOR_CHECK on
-                    Trigger { Sensors.DispatchSensor.isGreen } switchTo
-                    SCORING_LOW
-
-                COLOR_CHECK on
-                    !Trigger { Sensors.DispatchSensor.isGreen } switchTo
-                    SCORING_HIGH
-
-                SCORING_LOW.onComplete switchTo IDLE
-
-                [SCORING_LOW, SCORING_HIGH] on scoring.trigger switchTo IDLE
-
-                SCORING_HIGH.onComplete switchTo IDLE
+    companion object : StateMachineCompanion<State>(State::class) {
+        override val states = makeStates {
+            IDLE(idle())
+            INTAKING(intaking())
+            SCORING_HIGH(scoringHigh())
+            SCORING_LOW(scoringLow())
+            COLOR_CHECK {
+                park()
             }
+
+            allOf<State>() on RobotContainer.Buttons.intake switchTo INTAKING
+
+            INTAKING on !RobotContainer.Buttons.intake switchTo IDLE
+
+            IDLE on RobotContainer.Buttons.scoring.trigger switchTo ALIGNMENT
+            // TODO: Add trigger in position for scoring with 'and' operator
+
+            ALIGNMENT on RobotContainer.Buttons.scoring.trigger switchTo IDLE
+
+            ALIGNMENT.onComplete switchTo COLOR_CHECK
+
+            COLOR_CHECK on
+                Trigger { Sensors.DispatchSensor.isGreen } switchTo
+                SCORING_LOW
+
+            COLOR_CHECK on
+                !Trigger { Sensors.DispatchSensor.isGreen } switchTo
+                SCORING_HIGH
+
+            SCORING_LOW.onComplete switchTo IDLE
+
+            [SCORING_LOW, SCORING_HIGH] on
+                RobotContainer.Buttons.scoring.trigger switchTo
+                IDLE
+
+            SCORING_HIGH.onComplete switchTo IDLE
+        }
     }
 }
